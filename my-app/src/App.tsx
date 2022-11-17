8b6b4b2cd6a49e7a110e07d72dae9e9001625f82
bakanano <narutoz0rs@live.com>
2022-11-17 19:13:08 +1300

    Add axios module

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,30 @@
 import React, {useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
 
   const [pokemonName, setPokemonName] = useState("");
 
   function search() {
-    console.log("I've been clicked!");
+    setPokemonName("Piplup");
+    console.log(pokemonName);
   }
   
   return (
     <div className="App">
+
       <h1>Pokémon Search</h1>
 
       <section className="container">
+
         <label>Pokémon Name</label>
-        <input type="text" name="pokemon-name" onChange={e=> setPokemonName(e.target.value)}></input>
+        <input type="text" name="pokemon-name" 
+        onChange={e=> setPokemonName(e.target.value)}></input>
         <button onClick={search}>Search</button>
+
       </section>
+
     </div>
   );
 }
