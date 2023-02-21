d5ced24eef8ce13eafadb929ab48b8f693fb801a
bakanano <narutoz0rs@live.com>
2023-02-21 20:43:51 +1300

    Added background color based on pokemon type

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import axios from "axios";
 import {Pokemon} from "pokenode-ts";
 import SearchIcon from "@mui/icons-material/Search";
-import IconButton from "@mui/material/IconButton";
+import {IconButton, TextField, Card} from "@mui/material/";
 
 function App() {
 
@@ -36,7 +36,53 @@ function App() {
       }
       return pokemonInfo.held_items.map((heldItem: any) => (heldItem.item.name));
     }
-  } 
+  }
+  
+  function getBackgroundColor(poke: Pokemon | undefined | null) {
+    let backgroundColor = "#EEE8AA";
+    if (poke === undefined || poke === null) {
+      return backgroundColor;
+    }
+    let pokemonTypes = poke.types.map((pokeType) => pokeType.type.name);
+    if (pokemonTypes.includes("normal")) {
+      backgroundColor = "#A8A77A";
+    } else if (pokemonTypes.includes("fire")) {
+      backgroundColor = "#EE8130";
+    } else if (pokemonTypes.includes("water")) {
+      backgroundColor = "#6390F0";
+    } else if (pokemonTypes.includes("electric")) {
+      backgroundColor = "#F7D02C";
+    } else if (pokemonTypes.includes("grass")) {
+      backgroundColor = "#7AC74C";
+    } else if (pokemonTypes.includes("ice")) {
+      backgroundColor = "#96D9D6";
+    } else if (pokemonTypes.includes("fighting")) {
+      backgroundColor = "#C22E28";
+    } else if (pokemonTypes.includes("poison")) {
+      backgroundColor = "#A33EA1";
+    } else if (pokemonTypes.includes("ground")) {
+      backgroundColor = "#E2BF65";
+    } else if (pokemonTypes.includes("flying")) {
+      backgroundColor = "#A98FF3";
+    } else if (pokemonTypes.includes("psychic")) {
+      backgroundColor = "#F95587";
+    } else if (pokemonTypes.includes("bug")) {
+      backgroundColor = "#A6B91A";
+    } else if (pokemonTypes.includes("rock")) {
+      backgroundColor = "#B6A136";
+    } else if (pokemonTypes.includes("ghost")) {
+      backgroundColor = "#735797";
+    } else if (pokemonTypes.includes("dragon")) {
+      backgroundColor = "#6F35FC";
+    } else if (pokemonTypes.includes("dark")) {
+      backgroundColor = "#705746";
+    } else if (pokemonTypes.includes("steel")) {
+      backgroundColor = "#B7B7CE";
+    } else if (pokemonTypes.includes("fairy")) {
+      backgroundColor = "#D685AD";
+    }
+    return backgroundColor;
+  }
 
   return (
     <div className="App">
@@ -45,21 +91,39 @@ function App() {
 
       <section className="container">
 
-        <label>Pokémon Name</label>
-        <input type="text" name="pokemon-name" 
-        onChange={e=> setPokemonName(e.target.value)}></input>
-        <button onClick={search}>Search</button>
-
+        <TextField 
+        type="text"
+        variant="outlined"
+        label="Enter a Pokémon"
+        placeholder="Search"
+        name="pokemon-name"
+        value={pokemonName}
+        onChange={(prop: any)=> setPokemonName(prop.target.value)}>
+        </TextField>
+        <IconButton
+        aria-label="search"
+        color="primary"
+        onClick={search}>
+          <SearchIcon></SearchIcon>
+        </IconButton>
       </section>
 
       {(pokemonInfo == undefined) ? 
-      (<p>Pokemon was not found, please enter a pokemon name.</p>) :
-      <section className="card">
-        <h1>{pokemonInfo.name.charAt(0).toUpperCase() + pokemonInfo.name.slice(1)}</h1>
-        <img src={pokemonInfo.sprites.other.dream_world.front_default}/>
-        <p>Type: {getPokemonType()}</p>
-        <p>Abilties: {getPokemonAbilities().toString()}</p>
-        <p>Held Item(s): {getHeldItem().toString()}</p>
+      (<div></div>) :
+      <section>
+        <Card sx={{backgroundColor: getBackgroundColor(pokemonInfo)}}>
+            {pokemonInfo === undefined || pokemonInfo === null ? (
+            <h1>Pokemon was not found, please enter a pokemon name.</h1>
+            ) : (
+              <div>
+                <h1>{pokemonInfo.name.charAt(0).toUpperCase() + pokemonInfo.name.slice(1)}</h1>
+                <img src={pokemonInfo.sprites.other.dream_world.front_default}/>
+                <p>Type: {getPokemonType()}</p>
+                <p>Abilties: {getPokemonAbilities().toString()}</p>
+                <p>Held Item(s): {getHeldItem().toString()}</p>
+              </div>
+            )}
+        </Card>
       </section>}
 
     </div>
